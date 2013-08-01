054811795d5c9730325e03dd2e15e7c180330ecd
mitermayer reis <mitermayer.reis@gmail.com>
2013-08-01 18:13:03 +1000

    no more method run

--- tests/tests/page/demo_page_test.js
+++ tests/tests/page/demo_page_test.js
@@ -12,19 +12,13 @@ registerTest ('Demo page test', {
             ok(this.containerElement,'An element with the id "container" exists.');
         })
 
-        // run statements are executed before the test happens
-        .run(function(){
-            this.containerElement.style.background = this.otherColor;
-        })
         .test ("Does container have a background color of "+ self.otherColor +" ?",function(){
+            this.containerElement.style.background = this.otherColor;
             equal(this.containerElement.style.background, this.otherColor, 'Container background should now be ' + self.otherColor );
         })
 
-        // run statements are executed before the test happens
-        .run(function(){
-            this.containerElement.style.background = this.startBackground;
-        })
         .test ("Have container background being restored to its previosu value?",function(){
+            this.containerElement.style.background = this.startBackground;
             equal(this.containerElement.style.background, this.startBackground, 'Container background should now be set as its start value');
         });
     }
